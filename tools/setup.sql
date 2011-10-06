8876829f0075d611fac6912ec2580532624679e2
Ben Russell <benrr101@csh.rit.edu>
2011-10-06 11:27:32 -0400

    Added new fields to the quarter table to enable better ical generation.

--- tools/setup.sql
+++ tools/setup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 03, 2011 at 02:10 PM
+-- Generation Time: Oct 06, 2011 at 11:21 AM
 -- Server version: 5.0.77
 -- PHP Version: 5.2.10
 
@@ -33,8 +33,9 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `quarter` smallint(5) unsigned NOT NULL COMMENT '5 digit quarter, we''re good until 6500AD',
   `title` varchar(50) NOT NULL COMMENT 'Course title',
   PRIMARY KEY  (`id`),
+  UNIQUE KEY `department_2` (`department`,`course`,`quarter`),
   KEY `department` (`department`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=105822 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=57730 ;
 
 -- --------------------------------------------------------
 
@@ -45,8 +46,23 @@ CREATE TABLE IF NOT EXISTS `courses` (
 CREATE TABLE IF NOT EXISTS `departments` (
   `id` smallint(4) unsigned zerofill NOT NULL,
   `title` varchar(30) NOT NULL,
-  `category` varchar(30) NOT NULL,
+  `school` tinyint(2) unsigned zerofill NOT NULL COMMENT 'FK to schools.id',
   PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quarters`
+--
+
+CREATE TABLE IF NOT EXISTS `quarters` (
+  `quarter` smallint(5) unsigned NOT NULL,
+  `start` date NOT NULL COMMENT 'starting date of the quarter',
+  `end` date NOT NULL COMMENT 'ending date of the quarter',
+  `breakstart` date NOT NULL COMMENT 'starting date of the break',
+  `breakend` date NOT NULL COMMENT 'ending date of the break',
+  PRIMARY KEY  (`quarter`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -61,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `schedulecourses` (
   `section` int(10) unsigned NOT NULL COMMENT 'FK to sections.id',
   PRIMARY KEY  (`id`),
   KEY `schedule` (`schedule`,`section`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1229434 ;
 
 -- --------------------------------------------------------
 
@@ -78,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `schedulenoncourses` (
   `schedule` int(10) unsigned NOT NULL COMMENT 'FK to schedules.id',
   PRIMARY KEY  (`id`),
   KEY `schedule` (`schedule`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=386437 ;
 
 -- --------------------------------------------------------
 
@@ -88,10 +104,28 @@ CREATE TABLE IF NOT EXISTS `schedulenoncourses` (
 
 CREATE TABLE IF NOT EXISTS `schedules` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `datecreated` datetime NOT NULL,
-  `datelastaccessed` datetime NOT NULL,
+  `oldid` varchar(7) character set latin1 collate latin1_general_cs NOT NULL COMMENT 'the old style schedule indexes',
+  `datecreated` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `datelastaccessed` timestamp NOT NULL default '0000-00-00 00:00:00' on update CURRENT_TIMESTAMP,
+  `startday` tinyint(1) unsigned NOT NULL default '1' COMMENT 'The day the schedule starts on 0=sunday',
+  `endday` tinyint(1) unsigned NOT NULL default '6' COMMENT 'End day of the schedule, 0=sun',
+  `starttime` smallint(4) unsigned zerofill NOT NULL default '0800' COMMENT 'The start time, military style',
+  `endtime` smallint(4) unsigned zerofill NOT NULL default '2200' COMMENT 'Schedule end time, military style',
+  PRIMARY KEY  (`id`),
+  KEY `oldid` (`oldid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=234270 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schools`
+--
+
+CREATE TABLE IF NOT EXISTS `schools` (
+  `id` tinyint(2) unsigned zerofill NOT NULL,
+  `title` varchar(30) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -108,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `sections` (
   `curenroll` tinyint(3) unsigned NOT NULL COMMENT 'current enrollment',
   PRIMARY KEY  (`id`),
   KEY `course` (`course`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=105822 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=124250 ;
 
 -- --------------------------------------------------------
 
@@ -120,8 +154,8 @@ CREATE TABLE IF NOT EXISTS `times` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `section` int(10) unsigned NOT NULL COMMENT 'FK to sections.id',
   `day` tinyint(1) unsigned NOT NULL COMMENT 'Day of the week 0=sunday ',
-  `start` smallint(4) unsigned zerofill NOT NULL COMMENT 'start time',
-  `end` smallint(4) unsigned zerofill NOT NULL COMMENT 'end time',
+  `start` smallint(4) unsigned NOT NULL COMMENT 'start time',
+  `end` smallint(4) unsigned NOT NULL COMMENT 'end time',
   `building` varchar(3) NOT NULL COMMENT 'building code *sigh*',
   `room` varchar(4) NOT NULL COMMENT 'room number',
   PRIMARY KEY  (`id`),
