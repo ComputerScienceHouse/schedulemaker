71af5bd2ad05bca8cbb809631d1974fa951ea04e
Ben Russell <benrr101@csh.rit.edu>
2011-10-22 02:09:30 -0400

    BUTTFUCKS. forgot to add the file that adds in error handling stuff.

---
+++ inc/ajaxError.php
@@ -0,0 +1,21 @@
+<?php
+////////////////////////////////////////////////////////////////////////////
+// AJAX ERROR HANDLING
+//
+// @file	inc/ajaxError.php
+// @author	Ben Russell (benrr101@csh.rit.edu)
+// @descrip	Defines an error handler for use in ajax call files. This handler
+//			will halt execution on any error and output a useful (to both the
+//			the user and developer) message in the form of jSON.
+////////////////////////////////////////////////////////////////////////////
+
+function errorHandler($errno, $errstr, $errfile, $errline) {
+	die(json_encode(array(
+		"error" => "php",
+		"msg"   => "A internal server error occurred",
+		"guru"  => $errstr,
+		"num"   => $errno,
+		"file"  => "{$errfile}:{$errline}"
+		)));
+}
+set_error_handler("errorHandler");
