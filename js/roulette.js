d75152fd64383263ecddb3b196717422640c6fcc
Ben Russell <benrr101@csh.rit.edu>
2011-11-26 17:51:14 -0500

    Styling up the roulette page. I'm not thrilled with the results, but it's not god-awful.

--- js/roulette.js
+++ js/roulette.js
@@ -10,7 +10,7 @@
 
 function spinRoulette() {
 	// Serialize the data from the restrictions form and send it in a POST request
-	$.post("./js/rouletteAjax.php", $('#restrictions').serialize(), function(data) {
+	$.post("./js/rouletteAjax.php", $('#parameters').serialize(), function(data) {
 		// Process the resulting code
 		try {		
 			jsonResult = eval(data);
@@ -23,7 +23,7 @@ function spinRoulette() {
 		// Was there an error?
 		if(jsonResult.error != undefined && jsonResult.error != null) {
 			// Display the error in the result box
-			$('#rouletteCourse').html("<h2>Sorry! An Error Occurred!</h2><p>" + jsonResult.msg + "</p>");
+			$('#rouletteCourse').html("<h2>Sorry! An Error Occurred!</h2>" + jsonResult.msg + "");
 			$('#rouletteCourse').removeClass();
 			$('#rouletteCourse').addClass('rouletteError');
 		} else {
@@ -31,27 +31,29 @@ function spinRoulette() {
 			courseDiv.empty();
 			$("<h2>").html("Your Random Course")
 					.appendTo(courseDiv);
+			var title = jsonResult.department + "-" + jsonResult.course + "-" + jsonResult.section
+					+ " " + jsonResult.title + " with " + jsonResult.instructor;
 			$("<p>").css("font-weight", "bold")
-					.html(jsonResult.department + "-" + jsonResult.course + "-" + jsonResult.section)
+					.html(title)
 					.appendTo(courseDiv);
-			$("<p>").html(jsonResult.title + " with " + jsonResult.instructor)
-					.appendTo(courseDiv);
-			table = $("<table>").attr("id", "rouletteCourseTimes");
-			for(i = 0; i < jsonResult.times.length; i++) {
-				row = $("<tr>");
-				$("<td>").html(jsonResult.times[i].day)
-						.appendTo(row);
-				$("<td>").html(jsonResult.times[i].start)
-						.appendTo(row);
-				$("<td>").html("-")
-						.appendTo(row);
-				$("<td>").html(jsonResult.times[i].end)
-						.appendTo(row);
-				$("<td>").html(jsonResult.times[i].bldg + "-" + jsonResult.times[i].room)
-						.appendTo(row);
-				row.appendTo(table);
+			if(jsonResult.times.length > 0) {
+				table = $("<table>").attr("id", "rouletteCourseTimes");
+				for(i = 0; i < jsonResult.times.length; i++) {
+					row = $("<tr>");
+					$("<td>").html(jsonResult.times[i].day)
+							.appendTo(row);
+					$("<td>").html(jsonResult.times[i].start)
+							.appendTo(row);
+					$("<td>").html("-")
+							.appendTo(row);
+					$("<td>").html(jsonResult.times[i].end)
+							.appendTo(row);
+					$("<td>").html(jsonResult.times[i].bldg + "-" + jsonResult.times[i].room)
+							.appendTo(row);
+					row.appendTo(table);
+				}
+				table.appendTo(courseDiv);
 			}
-			table.appendTo(courseDiv);			
 
 			// @TODO:	Link to course in SIS
 			
@@ -69,7 +71,7 @@ function spinRoulette() {
 
 		// Make it visible and change the button text
 		$('#rouletteCourse').slideDown();
-		$('#spinButton').val("Respin!");
+		$('#spinButton').val("I Want a Different Course!");
 	});
 }
 
--- roulette.php
+++ roulette.php
@@ -13,38 +13,51 @@ require "./inc/header.inc";
 ?>
 <script type='text/javascript' src='./js/roulette.js'></script>
 
-<h1>Course Roulette</h1>
-<form id='restrictions' name='restrictions' action='roulette.php' method='POST'>
-<input type='hidden' name='action' value='rouletteSpin' />
-
-<table id='rouletteForm'>
-	<tr><td colspan='6'><h2>Refine the course list:</h2></td></tr>
-	<tr>
-		<td class='lbl'><label for='quarter'>Quarter*:</label></td>
-		<td><?= getQuarterField('quarter', $CURRENT_QUARTER) ?></td>
-		<td class='lbl'><label for='school'>College:</label></td>
-		<td><?= getCollegeField('school', null, true) ?></td>
-		<td class='lbl'><span style='font-weight:bold'>OR</span> <label for='department'>Department:</label></td>
-		<td><?= getDepartmentField('department', null, true) ?></td>
-		<td class='lbl'<label for='level'>Level:</label></td>
-		<td>
+<form id='parameters'>
+<div id='rouletteParams'>
+	<input type="hidden" value="rouletteSpin" name="action">
+	<div id='rouletteHeader'>
+		<h2>Set the Class Parameters</h2>
+		<span class='disclaimer'>* Denotes required field</span>
+	</div>
+	<div id='rouletteParamFields'>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='quarter'>Quarter*:</label></div>
+		<div class='rouletteField'><?= getQuarterField('quarter', $CURRENT_QUARTER) ?></div>
+	</div>
+	<div class='rouletteParamField' style='margin-bottom:0'>
+		<div class='rouletteLabel'><label for='college'>College:</label></div>
+		<div class='rouletteField'><?= getCollegeField('college', null, true) ?></div>
+	</div>
+	<div class='rouletteParamField'>OR</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='department'>Department:</label></div>
+		<div class='rouletteField'><?= getDepartmentField('department', null, true) ?></div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='level'>Level:</label></div>
+		<div class='rouletteField'>
 			<select name='level'>
 				<option value='any'>Any Level</option>
 				<option value='beg'>Introductory (0 - 300)</option>
 				<option value='int'>Intermediate (300 - 600)</option>
-				<option value='grad'>Graduate (>600)</option>
+				<option value='grad'>Graduate (&gt;600)</option>
 			</select>
-		</td>
-	</tr>
-	<tr>
-		<td class='lbl'><label for='credits'>Credit Hours:</label></td>
-		<td><input type='text' name='credits' size='3' maxlength='2' /></td>
-		<td class='lbl'><label for='professor'>Professor:</label></td>
-		<td><input type='text' name='professor' /></td>
-		<td class='lbl'><label for='days'>Days:</label></td>
-		<td>
-			<table style='font-size:12px; width:100%; border-collapse:collapse'>
-				<tr style="border-bottom:solid 1px grey;"><td><input type='checkbox' name='daysAny' value='any' onChange='toggleDaysAny(this)' /></td><td colspan='3'>Any Day</td></tr>
+		</div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='credits'>Credit Hours:</label></div>
+		<div class='rouletteField'><input type='text' name='credits' size='3' maxlength='2' /></div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='professor'>Professor:</label></div>
+		<div class='rouletteField'><input type='text' name='professor' /></div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'>Days:</div>
+		<div class='rouletteField'>
+			<table>
+				<tr style="border-bottom:solid 1px #0C5DA5;"><td><input type='checkbox' name='daysAny' value='any' onChange='toggleDaysAny(this)' /></td><td colspan='3'>Any Day</td></tr>
 				<tr>
 					<td><input id='mon' type='checkbox' name='days[]' value='Mon' /></td><td>Monday</td>
 					<td><input id='tue' type='checkbox' name='days[]' value='Tue' /></td><td>Tuesday</td>
@@ -56,25 +69,38 @@ require "./inc/header.inc";
 					<td><input id='sat' type='checkbox' name='days[]' value='Sat' /></td><td>Saturday</td>
 				</tr>
 			</table>
-		</td>
-		<td class='lbl'><label for='times'>Times:</label></td>
-		<td>
-			<table style='font-size:12px; width:100%; border-collapse:collapse'>
-				<tr style='border-bottom:solid 1px grey'><td><input type='checkbox' name='timesAny' value='any' onChange='toggleTimesAny(this)' /></td><td>Any Time</td></tr>
+		</div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'>Times:</div>
+		<div class='rouletteField'>
+			<table>
+				<tr style='border-bottom:1px solid #0C5DA5'><td><input type='checkbox' name='timesAny' value='any' onChange='toggleTimesAny(this)' /></td><td>Any Time</td></tr>
 				<tr><td><input id='morn' type='checkbox' name='times[]' value='morn' /></td><td>Morning (8am - noon)</td></tr>
 				<tr><td><input id='aftn' type='checkbox' name='times[]' value='aftn' /></td><td>Afternoon (noon - 5pm)</td></tr>
 				<tr><td><input id='even' type='checkbox' name='times[]' value='even' /></td><td>Morning (after 5pm)</td></tr>
 			</table>
-		</td>
-	</tr>
-	<tr><td colspan='6' style='font-size:10px'>* denotes required fields</td></tr>
-</table>
-<div id='rouletteImg'>BIG IMAGE GOES HERE</div>
+		</div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='online'>Include Online Courses:</label></div>
+		<div class='rouletteField'><input type='checkbox' name='online' value='true' checked='checked'></div>
+	</div>
+	<div class='rouletteParamField'>
+		<div class='rouletteLabel'><label for='honors'>Include Honors Courses:</label></div>
+		<div class='rouletteField'><input type='checkbox' name='honors' value='true' checked='checked'></div>
+	</div>
+	</div>
+</div>
+	
+
 <div id='rouletteCourse'></div>
 <div id='rouletteSpin'>
-		<input id='spinButton' type='button' value="Spin The Wheel" onClick='spinRoulette();' />
-		<noscript><input id='spinButton' type='submit' value="Spin The Wheel" /></noscript>
+	<input id='spinButton' class='bigButton' type='button' value="Give Me a Random Course!" onClick="spinRoulette();" />
 </div>
+
 </form>
 
-<? require "./inc/footer.inc"; ?>
+<?
+require "inc/footer.inc";
+?>
