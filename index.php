da1957ee2628a1d7ddd846ccfc842c99bae7e40e
Ben Russell <benrr101@csh.rit.edu>
2011-11-29 00:15:02 -0500

    Styled up the home page. That just about does it.

--- index.php
+++ index.php
@@ -18,38 +18,42 @@ if(isset($_GET['s'])) {
 
 require "./inc/header.inc";
 ?>
-<h1>Schedule Maker</h1>
 <div id="mainMenu">
 	<div class='navItem'>
-		<!-- image goes here -->
-		<a href='generate.php'>Make a Schedule</a>
+		<a href='generate.php'><img src='img/calendar.png' alt='Make a Schedule'></a>
+		<div><a href='generate.php'>Make a Schedule</a></div>
 	</div>
 	<div class='navItem'>
-		<img src='./img/browse.png' />
-		<a href='browse.php'>Browse Courses</a>
+		<a href='browse.php'><img src='img/browse.png' alt='Browse Courses'></a>
+		<div><a href='browse.php'>Browse Courses</a></div>
 	</div>
 	<div class='navItem'>
-		<img src='./img/roulette.png' />
-		<a href='roulette.php'>Course Roulette</a>
+		<a href='roulette.php'><img src='img/roulette.png' alt='Course Roulette'></a>
+		<div><a href='roulette.php'>Course Roulette</a></div>
 	</div>
 </div>
-<h2>Project Progress</h2>
-<ul>
-	<li>Index Page/Styling - <span class='b'>Partial</span></li>
-	<li>Schedule Form - <span class='c'>COMPLETE (08-12-11)</span></li>
-	<li>Schedule Generator - <span class='c'>COMPLETE (09-01-11)</span></li>
-	<li>Course Roulette - <span class='c'>COMPLETE (08-06-11)</span></li>
-	<li>Cronjob Status - Not complete</li>
-	<li>AJAX Integration - <span class='c'>COMPLETE</span></li>
-	<li>Courses DB - <span class='c'>COMPLETE (08-03-11)</span></li>
-	<li>Saved Schedule Lookup - <span class='c'>COMPLETE (10-07-11)</span></li>
-	<li>Saved Schedule Cleaner - <span class='c'>COMPLETE (10-07-11)</span></li>
-	<li>Schedule Output - <span class='c'>COMPLETE (09-17-11)</span></li>
-	<li>Social Media Sharing - <span class='c'>COMPLETE (09-17-11)</span></li>
-	<li>iCal Exporting - <span class='b'>Partial</span></li>
-	<li>Browse Courses - <span class='c'>COMPLETE (10-08-11)</span></li>
-	<li>Scraper Migration - <span class='c'>COMPLETE (10-13-11)</li>
-	<li>Migrator: Courses - <span class='c'>COMPLETE (08-03-11)</span></li>
-	<li>Migrator: Saves Schedules - <span class='c'>COMPLETE (08-04-11)</span></li>
-</ul>
+<div id="progress">
+	<h2>Project Progress</h2>
+	<div class='schedNotes'>
+		Follow this project at <a href='http://github.com/benrr101/schedulemaker'>http://github.com/benrr101/schedulemaker</a>
+	</div>
+	<ul>
+		<li>Index Page/Styling - <span class='b'>Partial</span></li>
+		<li>Schedule Form - <span class='c'>COMPLETE (08-12-11)</span></li>
+		<li>Schedule Generator - <span class='c'>COMPLETE (09-01-11)</span></li>
+		<li>Course Roulette - <span class='c'>COMPLETE (08-06-11)</span></li>
+		<li>Cronjob Status - Not complete</li>
+		<li>AJAX Integration - <span class='c'>COMPLETE</span></li>
+		<li>Courses DB - <span class='c'>COMPLETE (08-03-11)</span></li>
+		<li>Saved Schedule Lookup - <span class='c'>COMPLETE (10-07-11)</span></li>
+		<li>Saved Schedule Cleaner - <span class='c'>COMPLETE (10-07-11)</span></li>
+		<li>Schedule Output - <span class='c'>COMPLETE (09-17-11)</span></li>
+		<li>Social Media Sharing - <span class='c'>COMPLETE (09-17-11)</span></li>
+		<li>iCal Exporting - <span class='b'>Partial</span></li>
+		<li>Browse Courses - <span class='c'>COMPLETE (10-08-11)</span></li>
+		<li>Scraper Migration - <span class='c'>COMPLETE (10-13-11)</li>
+		<li>Migrator: Courses - <span class='c'>COMPLETE (08-03-11)</span></li>
+		<li>Migrator: Saves Schedules - <span class='c'>COMPLETE (08-04-11)</span></li>
+	</ul>
+</div>
 <? require "./inc/footer.inc"; ?>
