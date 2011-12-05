beab55f274e5657b4a14029b982180f9e06fcbd4
Ben Russell <benrr101@csh.rit.edu>
2011-12-04 19:28:59 -0500

    Sadface no more

---
+++ status.php
@@ -0,0 +1,22 @@
+<?php
+////////////////////////////////////////////////////////////////////////////
+// STATUS
+//
+// @file	status.php
+// @descrip	IT ALWAYS WORKS!!!
+// @author	Ben Russell (benrr101@csh.rit.edu)
+////////////////////////////////////////////////////////////////////////////
+
+require "inc/header.inc";
+?>
+
+<div id='status' class='working'>IT WORKS</div>
+<div class='schedNotes'>
+	<p>
+		Don't think so? Submit an issue at <a href='https://github.com/benrr101/schedulemaker/issues'>https://github.com/benrr101/schedulemaker/issues</a>
+	</p>
+</div>
+
+<?
+require "inc/footer.inc";
+?>
