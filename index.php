c1717d2442b77b36c6fd7d602819020d052b6a7e
Ben Russell <benrr101@csh.rit.edu>
2011-12-24 18:33:56 -0500

    For all the haters. Status is on status page. Copy and Edit.

--- index.php
+++ index.php
@@ -32,28 +32,4 @@ require "./inc/header.inc";
 		<div><a href='roulette.php'>Course Roulette</a></div>
 	</div>
 </div>
-<div id="progress">
-	<h2>Project Progress</h2>
-	<div class='schedNotes'>
-		Follow this project at <a href='http://github.com/benrr101/schedulemaker'>http://github.com/benrr101/schedulemaker</a>
-	</div>
-	<ul>
-		<li>Index Page/Styling - <span class='c'>COMPLETE (11-26-11)</span></li>
-		<li>Schedule Form - <span class='c'>COMPLETE (08-12-11)</span></li>
-		<li>Schedule Generator - <span class='c'>COMPLETE (09-01-11)</span></li>
-		<li>Course Roulette - <span class='c'>COMPLETE (08-06-11)</span></li>
-		<li>Cronjob Status - Not complete</li>
-		<li>AJAX Integration - <span class='c'>COMPLETE</span></li>
-		<li>Courses DB - <span class='c'>COMPLETE (08-03-11)</span></li>
-		<li>Saved Schedule Lookup - <span class='c'>COMPLETE (10-07-11)</span></li>
-		<li>Saved Schedule Cleaner - <span class='c'>COMPLETE (10-07-11)</span></li>
-		<li>Schedule Output - <span class='c'>COMPLETE (09-17-11)</span></li>
-		<li>Social Media Sharing - <span class='c'>COMPLETE (09-17-11)</span></li>
-		<li>iCal Exporting - <span class='b'>Partial</span></li>
-		<li>Browse Courses - <span class='c'>COMPLETE (10-08-11)</span></li>
-		<li>Scraper Migration - <span class='c'>COMPLETE (10-13-11)</span></li>
-		<li>Migrator: Courses - <span class='c'>COMPLETE (08-03-11)</span></li>
-		<li>Migrator: Saves Schedules - <span class='c'>COMPLETE (08-04-11)</span></li>
-	</ul>
-</div>
 <? require "./inc/footer.inc"; ?>
