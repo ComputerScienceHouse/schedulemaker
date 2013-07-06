d2aebaef9d43ec7d3220abb85bbcd6815632cc45
Benjamin Russell <benrr101@csh.rit.edu>
2013-07-05 22:25:15 -0700

    Rewriting the query for deleting old saved schedules. This new query utilizes the ON DELETEs in the schema for saved schedules Fixes #94

--- tools/pruneSchedules.php
+++ tools/pruneSchedules.php
@@ -13,7 +13,7 @@
 chdir(dirname($_SERVER['SCRIPT_FILENAME']));
 
 // Bring in the database connection
-require "../inc/databaseConn.php";
+require_once("../inc/databaseConn.php");
 global $dbConn;
 
 // Build a where clause that will be resued
@@ -21,19 +21,14 @@ $where = "WHERE (NOW() - datelastaccessed > (60 * 60 * 24 * 90))";
 
 // Run the query to delete the courses of the schedule that are older than
 // 90 days
-$query = "DELETE FROM schedulecourses WHERE schedule = (SELECT id FROM schedules {$where})";
-$result = mysql_query($query);
-if(!$result) { echo(mysql_error() . "\n"); }
-
-// Run the query to delete the non-course items of the schedule that are older
-// than 90 days
-$query = "DELETE FROM schedulenoncourses WHERE schedule = (SELECT id FROM schedules {$where})";
-$result = mysql_query($query);
-if(!$result) { echo(mysql_error() . "\n"); }
-
-// Run the query to delete the schedules that are older than 90 days
-$query = "DELETE FROM schedules {$where}";
-$result = mysql_query($query);
-if(!$result) { echo(mysql_error() . "\n"); }
+$ninetyDaysAgo = date("Y-m-d H:i:s", strtotime("-90 days"));
+$query = "DELETE FROM schedules WHERE datelastaccessed < '{$ninetyDaysAgo}'";
+$result = mysql_query($query, $dbConn);
+if(!$result) {
+    echo("*** Failed to run pruning query:\n");
+    echo(mysql_error() . "\n");
+} else {
+    echo("... " . mysql_affected_rows($dbConn) . " schedules deleted\n");
+}
 
 ?>
