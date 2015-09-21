ddf90e16a330bb80095e6f7160aa3b1a55373200
Joel Eager <jee7291@rit.edu>
2015-09-20 21:11:26 -0400

    Bug fix for last commit

--- assets/src/modules/sm/App/providers/RMPUrlFilter.js
+++ assets/src/modules/sm/App/providers/RMPUrlFilter.js
@@ -2,7 +2,7 @@
 angular.module('sm').filter('RMPUrl', function() {
 	return function(input) {
 		if(input && input != "TBA") {
-			var EscapedName = input.replace(/ /g, "+");
+			var EscapedName = encodeURIComponent(input);
 			return '<a target="_blank" href="http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&queryoption=HEADER&query=' + EscapedName + '&facetSearch=true&schoolName=rochester+institute+of+technology">' + input + '</a>';
 		} else {
 			return '<a href="#">' + input + '</a>';
