66e7e595000a7b03110eaf02fa468793875c68b3
Mary Strodl <ipadlover8322@gmail.com>
2024-03-19 11:34:13 -0400

    Use prepared statement for table insertion

--- tools/Parser.php
+++ tools/Parser.php
@@ -238,8 +238,9 @@ class Parser {
             }
 
             // Build a query
-            $insQuery = "INSERT INTO {$tableName} VALUES('" . implode("', '", $lineSplit) . "')";
-            if (!mysqli_query($this->dbConn, $insQuery)) {
+            $stmt = $this->dbConn->prepare("INSERT INTO {$tableName} VALUES(" . implode(", ", array_fill(0, $fields, "?")) . ")");
+            $stmt->bind_param(str_repeat("s", $fields), ...$lineSplit);
+            if (!$stmt->execute()) {
                 echo("*** Failed to insert {$tableName}\n");
                 echo("    " . mysqli_error($this->dbConn) . "\n");
                 continue;
