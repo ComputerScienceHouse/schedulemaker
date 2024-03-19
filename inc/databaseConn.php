4d61e8b32673550f54b834766e53885a49ed9a76
Mary Strodl <ipadlover8322@gmail.com>
2024-03-19 13:07:17 -0400

    set names?

--- inc/databaseConn.php
+++ inc/databaseConn.php
@@ -38,6 +38,7 @@ if (!$dbConn) {
 
 $dbConn->set_charset("utf8mb4");
 $dbConn->query("SET collation_connection = utf8mb4_unicode_ci");
+$dbConn->query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
 
 ////////////////////////////////////////////////////////////////////////////
 // FUNCTIONS
